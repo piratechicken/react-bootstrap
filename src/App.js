43b61d01c5ce54ee23409d75aa8e8f0f7a3a3191
Patrick Smith <patrick@burntcaramel.com>
2017-11-06 11:19:36 +1100

    Three simple buttons

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap-4.0.0-beta.2-dist/css/bootstrap.css'
+import Button from './components/Button'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Button title='First' />
+        <Button title='Second' />
+        <Button title='Third' />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({
+  title
+}) => (
+  <button className='btn btn-dark'>{ title }</button>
+)
+
+export default Button
