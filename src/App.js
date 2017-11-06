301b064b5fa50a980991ad483e2fdaaac6f608d2
Patrick Smith <patrick@burntcaramel.com>
2017-11-06 15:55:10 +1100

    Add Nav components
    # Conflicts:
    #	src/App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 import './bootstrap-4.0.0-beta.2-dist/css/bootstrap.css'
 import Button from './components/Button'
+import { Nav, NavItem } from './components/Nav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button title='Google' href='https://www.google.com/' />
-        <Button title='Second' variation='primary' />
-        <Button title='Third' variation='success' />
+        <div className='mt-3'>
+          <Button title='Google' href='https://www.google.com/' />
+          <Button title='Second' variation='primary' />
+          <Button title='Third' variation='success' />
+        </div>
+        <div className='mt-3'>
+          <Nav>
+            <NavItem>First</NavItem>
+            <NavItem active={ true }>Second</NavItem>
+            <NavItem>Third</NavItem>
+          </Nav>
+        </div>
       </div>
     );
   }
