dc2cdf88cdf043fb35818308f2c45fccaabc0366
Simbidion <simonadwyer@yahoo.com.au>
2017-11-27 15:01:43 +1100

    css clean up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import './bootstrap-4.0.0-beta.2-dist/css/bootstrap.css'
 import Button from './components/Button'
+import Alert from './components/Alert'
+import { Card, CardItem } from './components/Card'
 import { Nav, NavItem } from './components/Nav'
 
 class App extends Component {
@@ -20,6 +22,19 @@ class App extends Component {
             <NavItem active={ true }>Second</NavItem>
             <NavItem>Third</NavItem>
           </Nav>
+          <Alert subClass='primary'>
+            OMFG!!!
+          </Alert>
+          <Alert subClass='secondary'>
+            OMFG!!!
+          </Alert>
+          <Alert subClass='danger'>
+            OMFG!!!
+          </Alert>
+          <Card>
+            <CardItem subClass='title'>Title</CardItem>
+            <CardItem subClass='text'>...T E X T...</CardItem>
+          </Card>
         </div>
       </div>
     );
